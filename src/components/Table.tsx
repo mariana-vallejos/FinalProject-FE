df78fc08456e27e82ce5679b822ff95656cce8b6
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-09-28 11:09:09 -0400

    feat: add font

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -18,7 +18,7 @@ export function Table<T extends Record<string, any>>({ data, columns }: TablePro
                 <thead className="bg-primary text-white">
                     <tr>
                         {columns.map((col) => (
-                        <th key={col.key as string} className="px-4 py-2 text-center font-semibold">
+                        <th key={col.key as string} className="px-4 py-2 text-center font-sans">
                             {col.header}
                         </th>
                         ))}
--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -6,12 +6,13 @@ function Dashboard() {
   return (
     <div className="min-h-screen bg-primary-bg px-6 py-10">
       <div className="flex items-center justify-between mb-6">
-        <h1 className="text-3xl font-bold text-gray-800">
+        <h1 className="font-title text-3xl font-bold text-gray-800">
           Movie Management
         </h1>
         <button
             onClick={() => console.log("Aca se renderiza para añadir movie")}
-            className="bg-primary hover:bg-blue-500 text-white px-6 py-2 rounded-3xl rounded-lg shadow transition-colors">
+            className="bg-primary hover:bg-blue-500 text-white px-6 py-2 rounded-3xl rounded-lg shadow transition-colors"
+        >
           Add New Movie
         </button>
       </div>
@@ -27,16 +28,18 @@ function Dashboard() {
               header: "Actions",
               render: () => (
                 <div className="flex justify-center gap-4">
-                  <button 
-                    onClick={() => console.log("Aca se renderiza para editar")}
-                    className="text-gray-500 hover:text-blue-600 transition-colors">
-                    <MdEdit size={20} />
-                  </button>
-                  <button
-                    onClick={() => console.log("Aca se renderiza para eliminar")}
-                    className="text-gray-500 hover:text-red-600 transition-colors">
-                    <MdDelete size={20} />
-                  </button>
+                    <button 
+                        onClick={() => console.log("Aca se renderiza para editar")}
+                        className="text-gray-500 hover:text-blue-600 transition-colors"
+                    >
+                        <MdEdit size={20} />
+                    </button>
+                    <button
+                        onClick={() => console.log("Aca se renderiza para eliminar")}
+                        className="text-gray-500 hover:text-red-600 transition-colors"
+                    >
+                        <MdDelete size={20} />
+                    </button>
                 </div>
               ),
             },
