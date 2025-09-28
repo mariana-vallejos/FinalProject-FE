1c6e99f34add4ee6c1560067eaf2bf180f5d4ba3
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-09-28 18:40:55 -0400

    feat: wrapped app with providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.tsx"
-import { BrowserRouter } from "react-router"
+import { StrictMode, use } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { UserProvider } from "./context/UserContext";
+import { MoviesProvider } from "./context/MoviesContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <MoviesProvider>
+          <App />
+        </MoviesProvider>
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
