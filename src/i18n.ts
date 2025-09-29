6deaa6c8a970baa8eeb2f5fc79ac0af06d6c5c73
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-09-29 09:22:22 -0400

    feat: add internalization and fix import

---
+++ src/i18n.ts
@@ -0,0 +1,5 @@
+export const i18n = {
+  toast: {
+    showToast: "Mostrar Toast",
+  },
+};
--- src/pages/guest/Home.tsx
+++ src/pages/guest/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Toast from "../../components/Toast";
+import { i18n } from "../../i18n";
 
 function Home() {
   const [showToast, setShowToast] = useState(false);
@@ -11,7 +12,7 @@ function Home() {
         onClick={() => setShowToast(true)}
         className="bg-blue-500 text-white px-4 py-2 rounded"
       >
-        Mostrar Toast
+        {i18n.toast.showToast}
       </button>
 
       {showToast && (
