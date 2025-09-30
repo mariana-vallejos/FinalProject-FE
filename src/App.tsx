a770319494e2fce4d5c814282f34360587d2e7e5
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-09-29 20:10:55 -0400

    refactor:  organize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { Route, Routes } from "react-router";
-import LoginPage from "./pages/login/LoginPage";
-import Dashboard from "./pages/admin/Dashboard";
-import Home from "./pages/guest/Home";
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return <>
-    <Routes>
-      <Route path={'/'} element={<Home/>}/>
-      <Route path={'/login'} element={<LoginPage/>}/>
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute roles={["admin"]}>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <AppRoutes/>
   </>;
 }
 
