0e26f293c684c754f961fab051c80ee9fac6b4eb
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-09-30 09:34:39 -0400

    feat: implement route to movie details page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,23 +2,27 @@ import { Route, Routes } from "react-router";
 import LoginPage from "../pages/login/LoginPage";
 import Dashboard from "../pages/admin/Dashboard";
 import Home from "../pages/guest/Home";
+import MovieDetail from "../pages/movie/MovieDetail";
 import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
 
 function AppRoutes() {
-  return <>
-    <Routes>
-      <Route path={'/'} element={<Home/>}/>
-      <Route path={'/login'} element={<LoginPage/>}/>
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute roles={["admin"]}>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  </>;
+  return (
+    <>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/movies/:id"} element={<MovieDetail />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute roles={["admin"]}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default AppRoutes;
